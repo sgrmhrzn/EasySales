d747993ee727200e595b42453186d38e2fda0b03
Sagar <mahrzan_sgr@hotmail.com>
2017-01-25 09:42:42 +0545

    Easy Sale

--- GoodExample/obj/Release/Package/PackageTmp/Content/Angular/Module/Module.js
+++ GoodExample/obj/Release/Package/PackageTmp/Content/Angular/Module/Module.js
@@ -35,12 +35,12 @@
 
         angular.forEach(records, function (obj) {
             var receivedDate = obj.date;
-            //console.log(receivedDate);
+           console.log(receivedDate);
             if (receivedDate >= fromDate && receivedDate <= toDate) {
                 filtered.push(obj);
             }
         });
-        console.log(filtered);
+        //console.log(fromDate + '+' + toDate);
         return filtered;
     };
 }).filter('pagination', function () {
@@ -48,6 +48,25 @@
         start = +start;
         return input.slice(start);
     };
+}).factory('Excel',function($window){
+    var uri='data:application/vnd.ms-excel;base64,',
+        template='<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>',
+        base64=function(s){return $window.btoa(unescape(encodeURIComponent(s)));},
+        format=function(s,c){return s.replace(/{(\w+)}/g,function(m,p){return c[p];})};
+    return {
+        tableToExcel:function(tableId,worksheetName){
+            var table=$(tableId),
+                ctx={worksheet:worksheetName,table:table.html()},
+                href=uri+base64(format(template,ctx));
+            return href;
+        }
+    };
 })
+    .controller('excelCtrl',function(Excel,$timeout,$scope){
+        $scope.exportToExcel=function(tableId){ // ex: '#my-table'
+            var exportHref=Excel.tableToExcel(tableId,'WireWorkbenchDataExport');
+            $timeout(function(){location.href=exportHref;},100); // trigger download
+        }
+    });
 
 
